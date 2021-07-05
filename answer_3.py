5d36574a1bc71ad618e0dd3e079826f111d392bc
Gopikrishnan <gopikrishvs@gmail.com>
2021-07-05 22:39:07 +0530

    'Solutions'

---
+++ answer_3.py
@@ -0,0 +1,29 @@
+
+def find_profit(price_chart, d):
+    profit = 0   
+    for i in range(d-1): 
+        highest = max(price_chart, key=price_chart.get)
+        profit += price_chart[highest]
+        price_chart.pop(highest)
+        for j in price_chart:
+            if price_chart[j] <= 5:
+                profit += 5
+                price_chart.pop(j)
+            break
+        for i in price_chart:
+            price_chart[i] -= 2  
+    return profit
+
+
+d = 4
+price_chart = {
+"Mango": 150,
+"Grape": 100,
+"Banana": 8,
+"Apple": 200,
+"Orange": 10
+}
+maximum_amount = find_profit(price_chart, d)
+print(maximum_amount)
+
+
