22662dd307a13259d058bd516707ecfa125d497e
Gopikrishnan <gopikrishvs@gmail.com>
2021-07-05 22:53:27 +0530

    'Solutions'

--- answer_3.py
+++ answer_3.py
@@ -1,15 +1,18 @@
 
 def find_profit(price_chart, d):
     profit = 0   
-    for i in range(d-1): 
-        highest = max(price_chart, key=price_chart.get)
-        profit += price_chart[highest]
-        price_chart.pop(highest)
-        for j in price_chart:
-            if price_chart[j] <= 5:
-                profit += 5
-                price_chart.pop(j)
-            break
+    for i in range(d): 
+        if any(v > 5 for v in iter(price_chart.values())):
+            highest = max(price_chart, key=price_chart.get)
+            profit += price_chart[highest]
+            price_chart.pop(highest)
+        else:
+            for j in price_chart:
+                if price_chart[j] <= 5:
+                    profit += 5
+                    price_chart.pop(j)
+                break
+
         for i in price_chart:
             price_chart[i] -= 2  
     return profit
